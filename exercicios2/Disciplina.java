de75b96d1a5c3d8503ccaa91b7cb24675c8da114
Ryan Paulista <162994389+ryanpaulista@users.noreply.github.com>
2024-12-11 13:17:24 +0000

    Disciplina

--- exercicios2/Disciplina.java
+++ exercicios2/Disciplina.java
@@ -1,13 +1,17 @@
 public class Disciplina{
 
-    private String disciplina;
+    private String nomeDisciplina;
     private double mediaparcial;
     private double mediafinal;
 
     public Disciplina(){ 
-        this.disciplina = "";
+        this.nomeDisciplina = "";
         this.mediaparcial = 0;
         this.mediafinal = 0;
+    }
+
+    public void nomeDisciplina(String disciplina){
+        this.nomeDisciplina += disciplina;
     }
 
     public void CalcularMediaParcial(double bimestre1, double bimestre2){
@@ -17,6 +21,10 @@ public class Disciplina{
     
     public void CalcularMediaFinal(double mediaparcial, double provafinal){
         this.mediafinal = this.mediafinal + ((provafinal+mediaparcial)/2);
+    }
+
+    public String getDisciplina(){
+        return this.nomeDisciplina;
     }
 
     public double getMediaParcial(){
--- exercicios2/MainDisciplina.java
+++ exercicios2/MainDisciplina.java
@@ -7,19 +7,36 @@ public class MainDisciplina{
         Scanner sc = new Scanner(System.in);
 
         double bimestre1, bimestre2, provafinal; 
+        String nomeDisciplina1;
 
         Disciplina disciplina1 = new Disciplina();
 
+        System.out.print("Nome da disciplina: ");
+        nomeDisciplina1 = sc.nextLine();
+        System.out.print("Bimestre 1: ");
         bimestre1 = sc.nextDouble();
+        System.out.print("Bimestre 2: ");
         bimestre2 = sc.nextDouble();
 
         disciplina1.CalcularMediaParcial(bimestre1, bimestre2);
+        disciplina1.nomeDisciplina(nomeDisciplina1);
         double mediaparcial = disciplina1.getMediaParcial();
+
+        System.out.println(disciplina1.getDisciplina()); 
         System.out.println(disciplina1.getMediaParcial());
+
         if (mediaparcial<60){
+            System.out.print("O aluno ficou abaixo da média. Digite a nota da prova final: ");
             provafinal = sc.nextDouble(); 
             disciplina1.CalcularMediaFinal(mediaparcial, provafinal); 
-            System.out.println(disciplina1.getMediaFinal());  
+            if(disciplina1.getMediaFinal()<60){
+                System.out.print("Reprovou. Média final: ");
+                System.out.println(disciplina1.getMediaFinal());  
+            } else{
+                System.out.print("Aprovado. Média final: ");
+                System.out.println(disciplina1.getMediaFinal());  
+            }
+            
         }
 
     }
