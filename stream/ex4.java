6b1463cd422c5a73a60d361bc348615d3790c6b3
Ryan Paulista <162994389+ryanpaulista@users.noreply.github.com>
2025-01-28 09:42:38 -0300

    Create ex4.java

---
+++ stream/ex4.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List; 
+import java.util.stream.*;
+import java.util.stream.Collectors;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+        List<String> palavras = Arrays.asList("mão", "pés", "chão", "monitor");
+        String result = palavras.stream()
+            .collect(Collectors.joining(","));
+            
+        System.out.println(result);
+		
+	}
+}
